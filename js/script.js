13cc1005c26bcbaca5a42b989d483692151dff43
Alexander <alexmal74@gmail.com>
2022-09-01 01:44:13 +0500

    Встроил слайдер Swiperjs

---
+++ js/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const swiper = new Swiper(".mySwiper", {
+	slidesPerView: 4,
+	spaceBetween: 30,
+	slidesPerGroup: 4,
+	loop: true,
+	loopFillGroupWithBlank: true,
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+});
