135cf221e12d87f9dcc43446116195a8f0dfe265
Alexander <alexmal74@gmail.com>
2022-09-01 23:51:07 +0500

    адаптация под различные разрешения

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 const swiper = new Swiper(".mySwiper", {
 	slidesPerView: 4,
-	spaceBetween: 30,
+	spaceBetween: 16,
 	slidesPerGroup: 1,
 	loop: true,
 	loopFillGroupWithBlank: true,
@@ -14,4 +14,25 @@ const swiper = new Swiper(".mySwiper", {
 		nextEl: ".swiper-button-next",
 		prevEl: ".swiper-button-prev",
 	},
+	breakpoints: {
+		1500: {
+			slidesPerView: 4,
+			spaceBetween: 16
+		},
+		1200: {
+			slidesPerView: 4,
+		},
+
+		960: {
+			slidesPerView: 3
+		},
+
+		750: {
+			slidesPerView: 3
+		},
+
+		425: {
+			slidesPerView: 1
+		}
+	}
 });
